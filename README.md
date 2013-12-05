bc2c12d115c9ebd3b84e3074a975b1e0497eb983
Ethan Way <ethanway@Ethans-MacBook-Pro.local>
2013-12-04 21:59:53 -0500

    Wired up app with beefy and browserify.

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 Angular with Browserify
 =======================
+
+Sample app using angular with browserify.
+
+###To run:
+	- Go to root directory
+	- npm install
+	- npm start
+	- navigate to http://localhost:9966
--- app/js/index.js
+++ app/js/index.js
@@ -1,14 +1,22 @@
-var angular = require('./../../angular.js');
+'use strict';
+
+require('angular/angular');
+require('angular-route/angular-route');
 var controllers = require('./controllers');
 
 var app = angular.module('app', ['ngRoute']);
 
 app.config(['$routeProvider', '$httpProvider', 
 	function($routeProvider, $httpProvider) {
-
+		$routeProvider.when('/view1', {
+			templateUrl: './views/view1.html', 
+			controller: 'MyCtrl1'
+		}).when('/view2', {
+    	templateUrl: './views/view2.html', 
+    	controller: 'MyCtrl2'
+    }).otherwise({redirectTo: '/view1'});
 	}
-);
+]);
 
-app.controller('MyCtrl1', controllers.MyCtrl1);
-app.controller('MyCtrl1', controllers.MyCtrl2);
-
+app.controller('MyCtrl1', ['$scope', controllers.MyCtrl1]);
+app.controller('MyCtrl2', ['$scope', controllers.MyCtrl2]);
